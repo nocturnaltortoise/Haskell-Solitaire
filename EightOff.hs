45af76a37ccbe25b010741010589e91fcb6116e0
Simon Turner <simonturner34@gmail.com>
2015-11-21 19:09:21 +0000

    Almost working toFoundations

--- EightOff.hs
+++ EightOff.hs
@@ -24,98 +24,100 @@ where
     type Foundations = [Deck]
     type EOBoard = (Columns, Reserves, Foundations)
 
-    -- does this need an auxiliary function to handle the Maybe?
+    -- Takes a card and returns the next card in the suit, unless it's a King
+    -- in which case there is no valid next card so return Nothing
     sCard :: Card -> Maybe Card
     sCard (pip, suit)
         | pip /= King = Just (succ pip, suit)
         | otherwise = Nothing
 
+    -- Takes a card and returns the previous card in the suit, unless it's an Ace
+    -- in which case there is no valid previous card so return Nothing
     pCard :: Card -> Maybe Card
     pCard (pip, suit)
         | pip /= Ace = Just (pred pip, suit)
         | otherwise = Nothing
 
+    -- Takes a card and returns whether it's an Ace
     isAce :: Card -> Bool
     isAce (pip,_) = pip == Ace
 
+    -- Takes a card and returns whether it's a King
     isKing :: Card -> Bool
     isKing (pip,_) = pip == King
 
+    -- Generates a list of all the cards
     pack :: Deck
     pack = suitList Clubs ++ suitList Diamonds ++ suitList Hearts ++ suitList Spades
 
+    -- Generates all the cards in a suit
     suitList :: Suit -> [Card]
     suitList suit = [(pip, suit) | pip <- pipList]
         where pipList = [Ace ..]
 
+    -- Shuffles a set of cards
     shuffle :: Deck -> Deck
     shuffle a =
         let sortedPack = sortBy (comparing snd) (zip cards randomNumList)
-            rng = mkStdGen 334232345 --need a way of generating a random seed so shuffles are different every time
+            rng = mkStdGen 334232345
+            -- arbitrary seed, shuffling not really random.
             cards = a
-            randomNumList = take 50 (randoms rng::[Int])
+            randomNumList = take 52 (randoms rng::[Int])
         in map fst sortedPack
 
+    -- Creates a Board from the shuffled pack.
     eoDeal :: EOBoard
     eoDeal =
-        let shuffledDeck = shuffle pack
-            reserves = take 4 shuffledDeck
-            foundations = []
-            columns = splitDeck (drop 4 shuffledDeck) -- this could be replaced by a call to a function that split the shuffled deck into two lists (or a tuple?), one of 4 cards, and the second of 8 lists of 5
+        let shuffledDeck = shuffle pack -- Shuffle the pack of cards
+            reserves = take 4 shuffledDeck -- Take four out for the reserves
+            foundations = [] -- Make an empty list for the starting foundations
+            columns = splitDeck (drop 4 shuffledDeck) -- Split the remaining cards into 8 columns of 6
         in (columns,reserves,foundations)
-        --
+
+    -- Recursively split a deck of cards into lists of 6
     splitDeck :: Deck -> [Deck]
     splitDeck [] = []
     splitDeck deck = h : splitDeck t
         where (h,t) = splitAt 6 deck
 
+    --Helper function that makes the EOBoard a little easier to read.
     eoBoardToString :: EOBoard -> String
     eoBoardToString (columns,reserves,foundations) =
+        "Columns: " ++ show columns
+        ++ "Reserves: " ++ show reserves ++
         "Foundations: " ++ show foundations
-        ++ "Reserves: " ++ show reserves
-        ++ "Columns: " ++ show columns
 
-    toFoundationsA :: EOBoard -> EOBoard
-    toFoundationsA board@(columns,reserves,foundations) = (new_columns, new_reserves, new_foundations)
-        where new_foundations = map head (map (moveToFoundations board) topAces)
-              new_columns = removeFromColumns topAces board
-              new_reserves = dropWhile isAce reserves
-              topAces = filter isAce ((map head columns) ++ reserves)
-            --   ++ map (moveToFoundations (columns,reserves,new_foundations)) successorCards)
-              successorCards = findSuccessors (filter isAce (map head columns) ++ reserves)
-
- -- map (\e -> if ((e `elem` new_foundations) && (e `elem` columns)) then drop 1 (filter (==e) columns) else e) columns
-    removeFromColumns :: Deck -> EOBoard -> EOBoard
-    removeFromColumns deck board@(columns, reserves, foundations) = (new_columns, reserves, foundations)
-        where new_columns = [[]]
-
+    -- Recursively calls a helper function until there are no more legal moves.
     toFoundations :: EOBoard -> EOBoard
     toFoundations board
         | canMove board = toFoundations (toFoundationsA board)
         | otherwise = board
 
-    canMove :: EOBoard -> Bool
-    canMove board@(columns,reserves,foundations) = any isAce (filter isAce ((map head columns) ++ reserves))
+    -- Helper function that moves all the cards that can be moved without recursing down the columns
+    toFoundationsA :: EOBoard -> EOBoard
+    toFoundationsA board@(columns,reserves,foundations) = (new_columns, new_reserves, new_foundations)
+        where (_,_,new_foundations) = foldr moveToFoundations board (topAces ++ successorCards)
+              new_columns = filter (not.null) (filter (not . isAce . head) columns)-- need to include isSuccessor in here
+              new_reserves = filter (not . isAce) reserves
+              topAces = filter isAce (reserves ++ map head (filter (not . null) columns))
+              successorCards = findSuccessors (filter isAce (map head (columns ++ foundations)) ++ reserves)
+              -- perhaps this should call a function that returns a newly constructed board - with the successors removed from the columns and added to the foundations
 
+    -- Checks whether there are any cards that can be moved to the foundations.
+    canMove :: EOBoard -> Bool
+    canMove (columns,reserves,foundations) = any isAce ((map head columns) ++ reserves) || any isSuccessor (filter isAce (map head (filter (not.null) foundations)))
+
+    -- Finds successors cards of a list of cards.
     findSuccessors :: Deck -> Deck
+    findSuccessors [] = []
     findSuccessors [card] = [(fromJust . sCard) card]
     findSuccessors (h:t) = (fromJust . sCard) h : findSuccessors t
 
-    -- toFoundations board@(columns,reserves,_) = (new_columns,new_reserves,foundations)
-    --     where new_columns = map (moveToFoundations board) (filter isAce (map (head) columns))
-    --           new_reserves = map (moveToFoundations board) (filter isAce reserves)
+    -- Returns whether a successor of a card exists
+    isSuccessor :: Card -> Bool
+    isSuccessor = isJust . sCard
 
-    -- [ card | column <- columns, card <- column, isAce card]
-        -- check the head of the first column for an Ace
-            -- if there is an ace, move it to the foundations
-                -- now check the new head for a two, if there is one move it to a foundation with an ace of the same suit,
-                    -- and check the new head for a three ...
-            -- otherwise, check the next column for an ace
-
-    moveToFoundations :: EOBoard -> Card -> [Deck]
-    moveToFoundations board@(_,_,foundations) card
-        | null foundations && isAce card = [[card]]
-        | (isAce . head . head) foundations && card `notElem` (head foundations) = [reverse (card : head foundations)]
-        -- | otherwise = card : foundations
-        -- | null (head foundations) = card : head foundations
-        -- | otherwise = [card]
+    -- Moves a card to the foundations and returns the resulting EOBoard
+    moveToFoundations :: Card -> EOBoard -> EOBoard
+    moveToFoundations card (columns,reserves,foundations) = (columns, reserves, newFoundations)
+        where newFoundations = if null foundations then [[card]] else reverse [card : head foundations]
